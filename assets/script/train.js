083531b9e8beb3e3e637624d0f8d2763a30029ea
AngelMD55 <angel_demerutis@hotmail.com>
2018-12-05 21:36:40 -0700

    final

--- assets/script/train.js
+++ assets/script/train.js
@@ -1,45 +1,87 @@
-var list = []
+let trains = []
 
-function renderRow(list){
-    $('#tbody').empty();
-
-    for (var i = 0; i < list.length; i++){
-
-        var rowTraininfo = $('<p>');
-        rowTraininfo.text(list[i]);
-
-        $('#tbody').append(rowTraininfo);
-    }
-
-}
-
-
-$('#submitBtn').on('click', function(event){
+$('#submitBtn').on('click', function (event) {
 
     event.preventDefault();
 
-    var trainNameInput = $('#trainName').val().trim();
-    var destinationInput = $('#destination').val().trim();
-    var timeInput = $('#militaryTime').val().trim();
-    var frequencyInput = $('#frequency').val().trim();
+    let trainNameInput = $('#trainName').val().trim();
+    let destinationInput = $('#destination').val().trim();
+    let timeInput = moment($('#militaryTime').val().trim(), "HH:mm").format("HH:mm");
+    let frequencyInput = $('#frequency').val().trim();
 
-    var newRow = $( `<tr>
+    // console.log(timeInput);
+
+    let train = {
+        name: trainNameInput,
+        destination: destinationInput,
+        time: timeInput,
+        frequency: frequencyInput
+    }
+
+    trains.push(train);
+
+    // console.log(trains)
+
+    localStorage.setItem("Train-Info", JSON.stringify(trains));
+
+    let timeInputConverted = moment(timeInput, "HH:mm");
+    console.log(timeInputConverted);
+
+    let currentTime = moment();
+
+    console.log(currentTime.format("HH:mm"));
+
+    let timeDifference = moment().diff(moment(timeInputConverted), "minutes");
+    // console.log(timeDifference)
+
+    let timeRemaining = (timeDifference % frequencyInput);
+    console.log(timeRemaining)
+
+    let minutesAway = (frequencyInput - timeRemaining);
+    console.log(minutesAway)
+
+    let nextTrain = moment().add(minutesAway, "minutes").format("HH:mm");
+    console.log(nextTrain)
+
+
+    let newRow = $(`<tr>
     <td class='trainName'>${trainNameInput}</td>
     <td class='trainDestination'>${destinationInput}</td>
     <td class='trainFrequency'>${frequencyInput}</td>
-    <td class='trainTime'>${timeInput}</td>
+    <td class='trainTime'>${nextTrain}</td>
+    <td class='minutesAway'>${minutesAway}</td>
     </tr>` )
 
-    list.push(newRow)
-    
-    $('#tbody').append(newRow)
+    $('#tbody').append(newRow);
 
-    localStorage.setItem("Train-Info", (trainNameInput + " " + destinationInput + " " + timeInput + " " + frequencyInput));
-    $('#tbody').append(localStorage)
-
-    trainNameInput= $('#trainName').val('');
+    trainNameInput = $('#trainName').val('');
     destinationInput = $('#destination').val('');
     timeInput = $('#militaryTime').val('');
     frequencyInput = $('#frequency').val('');
-
 })
+trains = JSON.parse(localStorage.getItem("Train-Info"));
+populateTrains(trains);
+function populateTrains(trains) {
+    for (let i = 0; i < trains.length; i++) {
+
+        let timeDifference = moment().diff(moment(trains[i].time, "HH:mm"), "minutes");
+
+        let timeRemaining = (timeDifference % trains[i].frequency);
+        console.log(timeRemaining)
+
+        let minutesAway = (trains[i].frequency - timeRemaining);
+        console.log(minutesAway)
+
+        let nextTrain = moment().add(minutesAway, "minutes").format("HH:mm");
+        console.log(nextTrain)
+        let newRow = $(`<tr>
+        <td class='trainName'>${trains[i].name}</td>
+        <td class='trainDestination'>${trains[i].destination}</td>
+        <td class='trainFrequency'>${trains[i].frequency}</td>
+        <td class='trainTime'>${nextTrain}</td>
+        <td class='minutesAway'>${minutesAway}</td>
+        </tr>` )
+
+        $('#tbody').append(newRow);
+    }
+}
